9a12a5a6beeb28372addcd6b5610a25613e3402c
Mitch <Mitch@10.30.19.129>
2017-10-02 17:31:31 -0500

    Finishing Lab 3??.......

--- src/dip/lab3/student/solution1/Startup.java
+++ src/dip/lab3/student/solution1/Startup.java
@@ -9,27 +9,17 @@ public class Startup {
     public static void main(String[] args) {
         
         //Console Test
-//        MessageInputService testCI = new ConsoleMessageInput();
-//        String consoleMessageTest = testCI.getMessageInput();
-//        
-//        MessageOutputService testCO = new ConsoleMessageOutput();
-//        testCO.outputMessage(consoleMessageTest);
-//        
-//        
-//        //GUI Test
-//        MessageInputService testGI = new GuiMessageInput();
-//        String guiMessageTest = testGI.getMessageInput();
-//        
-//        MessageOutputService testGO = new GuiMessageOutput();
-//        testGO.outputMessage(guiMessageTest);
+        MessageService consoleMessageTest = new MessageService();
+        
+        String consoleInputTestString = consoleMessageTest.getConsoleMessageInput();
+        consoleMessageTest.consoleMessageOutput(consoleInputTestString);
         
         
-        
-        
+        //GUI Test
         MessageService guiMessageTest = new MessageService();
        
         String guiInputTestString = guiMessageTest.getGuiMessageInput();
         guiMessageTest.guiMessageOutput(guiInputTestString);
-
+        
     }
 }
